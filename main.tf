471ba7ba6242e9d71b76b576537e56ae47749307
Clairo Dorneles <clairo@clairodorneles.cloud>
2025-05-26 09:14:34 +0200

    Adds example of insecure storage account
    Adds an example of an insecure storage account to illustrate a potential security vulnerability.
    
    Also adds an azurerm provider block in version.tf.

--- main.tf
+++ main.tf
@@ -2,6 +2,16 @@
 #  Module: [Module Name] - Main
 #--------------------------------------------*--------------------------------------------
 
+# Example of a security issue: Storage Account without encryption
+resource "azurerm_storage_account" "insecure_storage" {
+  name                     = "insecurestorage"
+  resource_group_name      = "example-resources"
+  location                 = "West Europe"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  # Missing encryption settings
+}
+
 
 # end
 #--------------------------------------------*--------------------------------------------
